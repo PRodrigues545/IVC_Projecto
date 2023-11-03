e4a75cdef6a2218b045e73a603d44ef3becd5a84
Pedro Rodrigues <117277133+PRodrigues545@users.noreply.github.com>
2023-11-03 16:19:03 +0000

    Add files via upload

--- ProjetoIVC/breakout.py
+++ ProjetoIVC/breakout.py
@@ -238,64 +238,69 @@ game_over = False
 # Exit the program?
 exit_program = False
 
+def game_loop():
+    global exit_program, game_over, player, ball, blocks, screen, clock
 
-# Main program loop
-while not exit_program:
-    #chamar o loop da camara e encontrar o centro
-    center = camara.camara_loop()
+    # Main program loop
+    while not exit_program:
+        # chamar o loop da camara e encontrar o centro
+        center = camara.camara_loop()
 
-    # Limit to 30 fps
-    clock.tick(30)
+        # Limit to 30 fps
+        clock.tick(30)
 
-    # Clear the screen
-    screen.fill(black)
+        # Clear the screen
+        screen.fill(black)
 
-    # Process the events in the game
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit_program = True
+        # Process the events in the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_program = True
 
-    # Update the ball and player position as long
-    # as the game is not over.
-    if not game_over:
-        # Update the player and ball positions
-        player.update(center)
+        # Update the ball and player position as long
+        # as the game is not over.
+        if not game_over:
+            # Update the player and ball positions
+            player.update(center)
+            game_over = ball.update()
 
-        game_over = ball.update()
+        # If we are done, print game over
+        if game_over:
+            game_over = False
+            text = font.render("Game Over", True, white)
+            textpos = text.get_rect(centerx=background.get_width() / 2)
+            textpos.top = 300
+            screen.blit(text, textpos)
 
-    # If we are done, print game over
-    if game_over:
-        text = font.render("Game Over", True, white)
-        textpos = text.get_rect(centerx=background.get_width() / 2)
-        textpos.top = 300
-        screen.blit(text, textpos)
+        # See if the ball hits the player paddle
+        if pygame.sprite.spritecollide(player, balls, False):
+            # The 'diff' lets you try to bounce the ball left or right
+            # depending where on the paddle you hit it
+            diff = (player.rect.x + player.width / 2) - (ball.rect.x + ball.width / 2)
 
-    # See if the ball hits the player paddle
-    if pygame.sprite.spritecollide(player, balls, False):
-        # The 'diff' lets you try to bounce the ball left or right
-        # depending where on the paddle you hit it
-        diff = (player.rect.x + player.width / 2) - (ball.rect.x + ball.width / 2)
+            # Set the ball's y position in case
+            # we hit the ball on the edge of the paddle
+            ball.rect.y = screen.get_height() - player.rect.height - ball.rect.height - 1
+            ball.bounce(diff)
 
-        # Set the ball's y position in case
-        # we hit the ball on the edge of the paddle
-        ball.rect.y = screen.get_height() - player.rect.height - ball.rect.height - 1
-        ball.bounce(diff)
+        # Check for collisions between the ball and the blocks
+        deadblocks = pygame.sprite.spritecollide(ball, blocks, True)
 
-    # Check for collisions between the ball and the blocks
-    deadblocks = pygame.sprite.spritecollide(ball, blocks, True)
+        # If we actually hit a block, bounce the ball
+        if len(deadblocks) > 0:
+            ball.bounce(0)
 
-    # If we actually hit a block, bounce the ball
-    if len(deadblocks) > 0:
-        ball.bounce(0)
+            # Game ends if all the blocks are gone
+            if len(blocks) == 0:
+                game_over = True
 
-        # Game ends if all the blocks are gone
-        if len(blocks) == 0:
-            game_over = True
+        # Draw Everything
+        allsprites.draw(screen)
 
-    # Draw Everything
-    allsprites.draw(screen)
+        # Flip the screen and show what we've drawn
+        pygame.display.flip()
 
-    # Flip the screen and show what we've drawn
-    pygame.display.flip()
+# Call the game loop function to start the game
+game_loop()
 cv2.destroyWindow()
 pygame.quit()
